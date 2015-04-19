03ba81040f9f42aeccb946d9bdfdaef766dd44c9
mohsom <volodya.skalskyi@gmail.com>
2015-04-19 18:11:06 +0300

    First commit.

--- background.js
+++ background.js
@@ -2,10 +2,5 @@
  * Created by mohsom on 19.04.2015.
  */
 $("document").ready(function () {
-    var date = new Date();
-    var start_time = {
-        hours: date.getHours(),
-        minutes:date.getMinutes()
-    };
-    $("body").append(start_time.hours+":"+start_time.minutes);
+    
 });
